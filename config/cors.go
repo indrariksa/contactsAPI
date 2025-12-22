a83f8bafc88e7469371ec62c97a8a08dfa0cab76
indrariksa <indrariksa@gmail.com>
2025-12-22 15:28:31 +0700

    refactor CORS configuration to use AllowOriginsFunc for dynamic origin handling

--- config/cors.go
+++ config/cors.go
@@ -7,20 +7,20 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
-var origins = []string{
-	"http://127.0.0.1:5500",
-	"http://127.0.0.1:5501",
-	"http://127.0.0.1:8080",
-	"https://indrariksa.github.io",
-	"http://localhost:*",
-}
-
 var Internalhost string = os.Getenv("INTERNALHOST") + ":" + os.Getenv("PORT")
 
 var Cors = cors.Config{
-	AllowOrigins:     strings.Join(origins[:], ","),
 	AllowMethods:     "GET,HEAD,OPTIONS,POST,PUT,DELETE",
 	AllowHeaders:     "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Request-Headers, Token, Access-Control-Allow-Origin",
 	ExposeHeaders:    "Content-Length",
 	AllowCredentials: true,
+	AllowOriginsFunc: func(origin string) bool {
+		// izinkan github pages
+		if origin == "https://indrariksa.github.io" {
+			return true
+		}
+		// izinkan localhost / 127.0.0.1 port berapa pun (DEV)
+		return strings.HasPrefix(origin, "http://localhost:") ||
+			strings.HasPrefix(origin, "http://127.0.0.1:")
+	},
 }
